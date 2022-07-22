50b9a5f5677d3eddecf491db89b83b3743cdc33e
Mustafa Megahed <61358936+Mustafamegahed20@users.noreply.github.com>
2022-07-22 22:51:39 +0200

    sidebar_commit

--- lms/lms_app/views.py
+++ lms/lms_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import *
 from .forms import BookForm,CategoryForm
 # Create your views here.
@@ -27,3 +27,26 @@ def books(request):
     }     
     return render(request,'pages/books.html',context) 
 
+def update(request,id):
+    book_id=Book.objects.get(id=id)
+    if request.method == 'POST':
+        book_save=BookForm(request.POST,request.FILES,instance=book_id)
+        if book_save.is_valid():
+            book_save.save()
+            return redirect("/")
+    else:
+        book_save=BookForm(instance=book_id)
+    
+    context ={
+        'form':book_save,
+    }
+    
+    return render(request,'pages/update.html',context)
+
+def delete(request,id):
+    book_delete = get_object_or_404(Book,id=id)
+    if request.method == 'POST':
+        book_delete.delete()
+        return redirect('/')
+    return render(request,'pages/delete.html')
+    
